cf68ce3a167be6384e3e4eaa24e72455d6f8ee56
jeff15113 <itsmejeffery@gmail.com>
2019-02-11 18:24:44 -0500

    added post

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // implement your API here
+
+const express = require("express");
+
+const db = require("./data/db.js");
+
+const server = express();
+
+const port = 3333;
+
+server.use(express.json());
+
+server.post("/pi/users", (req, res) => {
+  const hub = req.body;
+
+  db.hubs
+    .add(hub)
+    .then(hub => {
+      res.status(201).json({ success: true, hub });
+    })
+    .catch(({ code, message }) => {
+      res.status(code).json({ success: false, message });
+    });
+});
+
+server.listen(port, () => {
+  console.log(`\n*** Running on port ${port} ***\n`);
+});
